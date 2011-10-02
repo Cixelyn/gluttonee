f021bb809592d1e1b4568eefb12cd20a18fda64b
Cory Li <coryli@mit.edu>
2011-10-02 05:55:12 -0400

    lots of basic javascript infrastructure put in place

--- gluttonee/static/application.coffee
+++ gluttonee/static/application.coffee
@@ -1,13 +1,102 @@
 window.GT = {}
 GT.home = ( ->
 
+  gWidth = 0
+  gHeight = 0
+  r = null
 
-  showInit = ->
-    1+1
+  sX = (val) ->
+    return val*gWidth
+
+  sY = (val) ->
+    return val*gHeight
+
+  centerImage = (src,x,y,w,h) ->
+    r.image(src,sX(x)-w/2,sY(y)-h/2,w,h)
+
+  handleKeyPress = (e) ->
+    switch e.which
+      when 13 then this.click()
+      when 27 then null # escape
+      when 32 then null # space
+      when 37 then this.left()
+      when 38 then this.up()
+      when 39 then this.right()
+      when 40 then this.down()
+
+
+
+  viewMainMenu = ( ->
+
+    intervalHandler = null
+
+    randomImage = () ->
+      img = centerImage('http://placekitten.com/100/100', Math.random(), Math.random(), 100, 100)
+      img.attr({opacity: '0.0'}).animate {opacity: '1.0' }, 2000, ->
+        img.animate {opacity: '0.0'}, 2000, ->
+          img.remove()
+
+    keyEvent = (e) ->
+      console.log('triggered!')
+      switch e.which
+        when 13
+          clearInterval(intervalHandler)
+          viewSelectVenue().exec()
+          eve.stop()
+          eve.unbind('key', keyEvent)
+
+    exec: () ->
+      intervalHandler = setInterval(randomImage, 1000)
+      eve.on('key', keyEvent)
+
+  )
+
+
+
+  viewSelectVenue = ( ->
+
+    listVenues = []
+    selectVenues = 0
+
+    addVenue = (img) ->
+      listVenues.push(img)
+
+    keyEvent = (e) ->
+      switch e.which
+        when 37
+          selectVenues = (selectVenues - 1) % listVenues.length
+        when 39
+          selectVenues = (selectVenues + 1) % listVenues.length
+
+
+    exec: () ->
+      eve.on('key', keyEvent)
+
+
+  )
+
+
+
+
+
+
+  init = ->
+    # initialize Raphael.js
+    gWidth = document.body.clientWidth
+    gHeight = document.body.clientHeight
+    r = Raphael('canvas', '100%', '100%')
+
+    # bind keypress handler
+    $(document).keypress (e) ->
+      e.preventDefault()
+      eve('key', null, e)
+
+    # begin executing from the main menu
+    viewMainMenu().exec()
 
 
   (
-    showInit: showInit
+    init: init
   )
 
 )()
--- gluttonee/static/application.js
+++ gluttonee/static/application.js
@@ -1,12 +1,105 @@
 (function() {
   window.GT = {};
   GT.home = (function() {
-    var showInit;
-    showInit = function() {
-      return 1 + 1;
+    var centerImage, gHeight, gWidth, handleKeyPress, init, r, sX, sY, viewMainMenu, viewSelectVenue;
+    gWidth = 0;
+    gHeight = 0;
+    r = null;
+    sX = function(val) {
+      return val * gWidth;
+    };
+    sY = function(val) {
+      return val * gHeight;
+    };
+    centerImage = function(src, x, y, w, h) {
+      return r.image(src, sX(x) - w / 2, sY(y) - h / 2, w, h);
+    };
+    handleKeyPress = function(e) {
+      switch (e.which) {
+        case 13:
+          return this.click();
+        case 27:
+          return null;
+        case 32:
+          return null;
+        case 37:
+          return this.left();
+        case 38:
+          return this.up();
+        case 39:
+          return this.right();
+        case 40:
+          return this.down();
+      }
+    };
+    viewMainMenu = (function() {
+      var intervalHandler, keyEvent, randomImage;
+      intervalHandler = null;
+      randomImage = function() {
+        var img;
+        img = centerImage('http://placekitten.com/100/100', Math.random(), Math.random(), 100, 100);
+        return img.attr({
+          opacity: '0.0'
+        }).animate({
+          opacity: '1.0'
+        }, 2000, function() {
+          return img.animate({
+            opacity: '0.0'
+          }, 2000, function() {
+            return img.remove();
+          });
+        });
+      };
+      keyEvent = function(e) {
+        console.log('triggered!');
+        switch (e.which) {
+          case 13:
+            clearInterval(intervalHandler);
+            viewSelectVenue().exec();
+            eve.stop();
+            return eve.unbind('key', keyEvent);
+        }
+      };
+      return {
+        exec: function() {
+          intervalHandler = setInterval(randomImage, 1000);
+          return eve.on('key', keyEvent);
+        }
+      };
+    });
+    viewSelectVenue = (function() {
+      var addVenue, keyEvent, listVenues, selectVenues;
+      listVenues = [];
+      selectVenues = 0;
+      addVenue = function(img) {
+        return listVenues.push(img);
+      };
+      keyEvent = function(e) {
+        switch (e.which) {
+          case 37:
+            return selectVenues = (selectVenues - 1) % listVenues.length;
+          case 39:
+            return selectVenues = (selectVenues + 1) % listVenues.length;
+        }
+      };
+      return {
+        exec: function() {
+          return eve.on('key', keyEvent);
+        }
+      };
+    });
+    init = function() {
+      gWidth = document.body.clientWidth;
+      gHeight = document.body.clientHeight;
+      r = Raphael('canvas', '100%', '100%');
+      $(document).keypress(function(e) {
+        e.preventDefault();
+        return eve('key', null, e);
+      });
+      return viewMainMenu().exec();
     };
     return {
-      showInit: showInit
+      init: init
     };
   })();
 }).call(this);
